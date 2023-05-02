a34b56b65e0c01fe2933f18b92cef4558c0b6cf6
Инна Романец <romanets_inna@outlook.com>
2023-05-02 07:06:26 +0300

    020523

--- source/js/main.js
+++ source/js/main.js
@@ -75,3 +75,38 @@ button.addEventListener('click', () => {
     iframeBlock.append(newIframe);
   }
 });
+
+// Слайдер
+
+const swiper = new window.Swiper('.swiper', {
+  // Default parameters
+  slidesPerView: 1,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  mousewheel: true,
+  keyboard: true,
+  spaceBetween: 10,
+  // Responsive breakpoints
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+  },
+});
